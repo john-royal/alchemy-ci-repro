7c21ddfb9e92cac88602579a64404eb92f608531
John Royal <34844819+john-royal@users.noreply.github.com>
2025-10-23 16:01:49 -0400

    does this restore the issue?

--- apps/frontend/alchemy.run.ts
+++ apps/frontend/alchemy.run.ts
@@ -12,6 +12,7 @@ export const worker = await ReactRouter("frontend", {
     SESSION_SECRET: alchemy.secret(process.env.SESSION_SECRET || "test-secret"),
     VALUE_FROM_CLOUDFLARE: alchemy.secret(process.env.VALUE_FROM_CLOUDFLARE || "Hello from Cloudflare"),
   },
+  compatibilityFlags: ["nodejs_compat", "nodejs_compat_populate_process_env"],
   url: true,
   adopt: true
 })
